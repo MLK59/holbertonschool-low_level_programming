fb0c72ecf4d5e5955892ae895f453b55c7402e15
MLK59 <abdelmalekmhamed@gmail.com>
2023-10-15 11:03:01 -0700

    P4T4 puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- * * main - print the string in the put function
- * *
- * * Description: using the main function
- * * this program prints "Programming is like building a multilingual puzzle
- * * Return: 0
- * */
+ * main - Displays the string using the puts function
+ *
+ * Description: Uses the main function to display
+ * "Programming is like building a multilingual puzzle".
+ *
+ * Return: Always returns 0 (success)
+ **/
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("Programming is like building a multilingual puzzle");
+		return (0);
 }

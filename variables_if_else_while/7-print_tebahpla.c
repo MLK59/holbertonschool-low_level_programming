a88686a615280a6a353d05d993df28b2c8588e64
MLK59 <abdelmalekmhamed@gmail.com>
2023-10-16 15:28:42 -0700

    reverse alpha

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet miniscule reverse
+ * Description: using the main function
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for  (c = 'z' ; c  >= 'a' ; c--)
+	{
+		putchar(c);
+	}
+
+		putchar('\n');
+		return (0);
+
+}

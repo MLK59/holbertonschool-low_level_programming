459dec0a48256e453f894ee3d13870ba5f0e16f1
MLK59 <abdelmalekmhamed@gmail.com>
2023-10-17 02:17:24 -0700

    print 0-9

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+ /**
+ * printing numbers from 0-9
+ * Description: using the main function
+ * Return: 0
+ */
+
+	int main(void)
+
+	{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	putchar(c);
+		}
+	putchar('\n');
+	return (0);
+	}

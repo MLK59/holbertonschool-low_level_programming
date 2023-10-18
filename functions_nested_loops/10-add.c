6d4579f7c41a1d335b9bb1e319e39d5130159039
MLK59 <abdelmalekmhamed@gmail.com>
2023-10-18 14:55:42 -0700

    adds two integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - additionne deux entiers
+ * a: premier entier
+ * b: deuxième entier
+ * Return: résultat de l'addition
+ */
+int add(int a, int b)
+{
+	    return a + b;
+}

be0d6e742fb6966a74f4b76ebc4cf698bfa18b1b
MLK59 <abdelmalekmhamed@gmail.com>
2023-10-22 12:49:19 -0700

    print 0, to ,9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
  /**
-  * main - printing numbers from 0-9 with ,  and space between them
-  * Description: using the main function
-  * Program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
-  * Return: 0
-  */
+ * main - Entry point, prints numbers from 0 to 9
+ * Description: Uses the main function
+ * main - printing numbers from 0-9 with ,  and space between them
+ * Description: using the main function
+ * Program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+ * Return: 0
+ */
 
 int main(void)
 

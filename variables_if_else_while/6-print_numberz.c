8f7576b9a3ca4dd4b03d35ee07951b518d4854e0
MLK59 <abdelmalekmhamed@gmail.com>
2023-10-22 12:44:24 -0700

    print 0-9

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
- /**
- * main - printing numbers from 0-9
- * Description: using the main function
- * @c: the int for the paramaters of my function
- * Return: 0
+/**
+ * main - Entry point, prints numbers from 0 to 9
+ * Description: Uses the main function to print numbers
+ * from 0 to 9 followed by a new line.
+ * Return: Always 0 (success)
  */
-
 	int main(void)
 
 	{
-	char c;
+	int c;
 
 	for (c = '0' ; c <= '9' ; c++)
 	{

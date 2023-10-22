35a8b68870cfb6550f75314a7f97d4d82b8a4a92
MLK59 <abdelmalekmhamed@gmail.com>
2023-10-22 14:27:06 -0700

    betty

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,11 +2,11 @@
 
 /**
  * add - additionne deux entiers
- * a: premier entier
- * b: deuxième entier
+ * @a: premier entier
+ * @b: deuxième entier
  * Return: résultat de l'addition
  */
 int add(int a, int b)
 {
-	    return a + b;
+	return (a + b);
 }

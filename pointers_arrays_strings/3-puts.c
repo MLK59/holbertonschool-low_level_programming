b5d3fc6a88a62fd569a3dea970abd5f80c31b65e
MLK59 <7811@holbertonstudents.com>
2023-10-24 13:38:22 -0700

    print string

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -4,15 +4,14 @@
  * _puts - Prints a string
  * @str: The input string to be printed.
  */
+
 void _puts(char *str)
 {
-	int c = 0;
+	int c;
 
-	while (str[c] != '\0')
+	for (c = 0; str[c] != 0; c++)
 	{
 		_putchar(str[c]);
-		str++;
 	}
-
 	_putchar('\n');
 }

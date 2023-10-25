8bf25e65704e421fcfe695bfc9094457e3dffb53
MLK59 <7811@holbertonstudents.com>
2023-10-25 05:31:38 -0700

    print the last n = (lengh -1)/2

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
  * puts_half - function that prints half of a string, followed by a new line.
- * @str: declaration of n and paramters for the function
+ * @str: declaration and paramters for the function
  * Return: 0
  */
 
 void puts_half(char *str)
 {
-	int c;
+	int c = 0;
 	int f;
 
-	for (c = 0; str[c] != 0; c++)
+	while (str[c] != '\0')
 	{
+		c++;
 	}
-		for (f = c / 2; str[f] != 0; f++)
+		for (f = (c + 1) / 2; f < c; f++)
 		{
 			putchar(str[f]);
 		}

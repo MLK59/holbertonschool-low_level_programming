ed39f224258a66d0912045e110e0037f848d95e3
MLK59 <7811@holbertonstudents.com>
2023-10-25 06:34:43 -0700

    T8 prints n elements of an array of integers

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_array - Prints n elts of an array of int, followed by a new line.
+* @a: Array of integers
+* @n: Number of elements to be printed
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
+
+

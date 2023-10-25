93a544b9157a027081c17ea7f7d5dcba35178b19
MLK59 <7811@holbertonstudents.com>
2023-10-25 02:23:12 -0700

    T4 reverse string

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,9 @@
 #include "main.h"
-#include <unistd.h>
 /**
- * print_rev -  Prints a string reverse
- * @str: The input string to be printed.
+ * print_rev - func that prints a string, in reverse, followed by a new line
+ * @s: declaration of s and paramters for the function
+ * Return: Always 0.
  */
-void _putchar (char c)
-{
-}
 void print_rev(char *s)
 {
 	int c;
@@ -14,7 +11,8 @@ void print_rev(char *s)
 	for (c = 0; s[c] != 0; c++)
 	{
 	}
-		for (c = c - 1; c >= 0; c--)
-			_putchar(s[c]);
+	for (c = c - 1; c >= 0; c--)
+		_putchar(s[c]);
 	_putchar('\n');
+
 }

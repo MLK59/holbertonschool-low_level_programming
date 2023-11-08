3f9be86be15c362799fc3a59416c33abe4965de7
MLK59 <7811@holbertonstudents.com>
2023-11-08 01:08:25 -0800

    main.h

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,6 +1,7 @@
-#ifndef HOLBERTON_H_FILE
-#define HOLBERTON_H_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
 char *create_array(unsigned int size, char c);
-
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 #endif

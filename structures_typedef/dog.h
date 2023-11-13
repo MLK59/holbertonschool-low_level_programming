878b44eef59e84cde68c91c5c95e6406ef271286
MLK59 <7811@holbertonstudents.com>
2023-11-13 01:43:42 -0800

    P1T0 Define a new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - caracterisée par name, age, owner.
+ * @name: nom
+ * @age: age
+ * @owner: proprietaire
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /** DOG_H */

13bda7ad4b8368c7e4f6dd16c1ca97989de4ba97
MLK59 <abdelmalekmhamed@gmail.com>
2023-11-29 02:24:02 +0100

    Function that returns the number of elements in a linked list_t list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,28 @@
+/**
+ * list_len - calculates the length of a linked list
+ * @h: pointer to the head of the list
+ * Return: the number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+
+    /*
+     * Variable to store the number of nodes
+     */
+    count = 0;
+
+    /*
+     * Iterate through the list, incrementing `count`
+		 * for each node encountered.
+     */
+		while (h != NULL)
+		{
+			count++;
+			h = h->next;
+    }
+
+    /*
+     * Return the value of `count`,
+		 * which represents the length of the linked list.
+     */
+		return (count);

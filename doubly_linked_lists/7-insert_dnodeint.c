8092d03f1ad86a15988b9bacca4d660aeb626686
MLK59 <abdelmalekmhamed@gmail.com>
2023-12-01 23:56:57 +0100

    upgrate file

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,84 +1,43 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a specified index in a doubly linked list
- * @h: pointer to the head pointer of the doubly linked list
- * @idx: the index at which to insert the new node
- * @n: the data to store in the new node
- *
- * Return: a pointer to the newly inserted node, or NULL if the insertion failed
+ * *insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: head of the structure
+ * @idx: index of the list where the new node should be added
+ * @n: number to be added
+ * Return: address of new node or NULL if it failed
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    if (idx == 0) {
-        /**
-         * Use the 'add_dnodeint' function for insertion at the beginning
-         */
-        return (add_dnodeint(h, n));
-    }
+	dlistint_t *node_current, *new_node;
+	unsigned int i = 0;
 
-    /**
-     * Set 'current_node' to point to the head node of the list
-     */
-    dlistint_t *current_node = *h;
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-    while (i < idx - 1)
+	node_current = *h;
+
+	while (i < idx - 1)
 	{
-        /**
-         * Check if the current node is NULL, indicating an invalid index
-         */
-        if (!current_node)
-		{
+		if (!node_current)
+			return (NULL);
+		node_current = node_current->next;
+		i++;
+	}
 
-            return (NULL);
-        }
+	if (!node_current->next)
+		return (add_dnodeint_end(h, n));
 
-        /**
-         * Move to the next node in the list
-         */
-        current_node = current_node->next;
-        i++;
-    }
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
 
-    /**
-     * Check if the next node is NULL, indicating insertion at the end of the list
-     */
-    if (!current_node->next)
-	{
-        /**
-         * Use the 'add_dnodeint_end' function for insertion at the end
-         */
-        return (add_dnodeint_end(h, n));
-    }
+	new_node->n = n;
+	new_node->next = node_current->next;
+	new_node->prev = node_current;
+	node_current->next->prev = new_node;
+	node_current->next = new_node;
 
-    /**
-     * Allocate memory for the new node
-     */
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
-    if (!new_node) {
-
-        return (NULL);
-    }
-
-    /**
-     * Set the data for the new node
-     */
-    new_node->n = n;
-
-    /**
-     * Link the new node into the doubly linked list
-     */
-    new_node->next = current_node->next;
-	/** Connect the new node to the next node in the list */
-    new_node->prev = current_node; 
-	/**Connect the new node to the previous node in the list */
-    current_node->next->prev = new_node;
-	/**Connect the next node to the new node */
-    current_node->next = new_node;
-	/** Connect the previous node to the new node */
-
-    /**
-     * Return a pointer to the newly inserted node
-     */
-    return (new_node);
+	return (new_node);
 }
